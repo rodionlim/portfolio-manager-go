f57c148ceae0640b1cba17d468b5b4586fce989f
Rodion Lim <rodion.lim@hotmail.com>
2024-12-22 19:13:23 +0800

    feat: add web-ui skeleton

---
+++ web/ui/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@mantine/core";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Portfolio Manager</h1>
+      <Button>Hello Mantine</Button>
+    </div>
+  );
+}
+
+export default App;
